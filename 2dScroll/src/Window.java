0ce79243a25b6c1cc8f97ae659430adcced29cd6
Ricard Skrudlik <ricardskrudlik34@gmail.com>
2022-12-05 12:10:48 +0100

    Add Frame extension

--- 2dScroll/src/Window.java
+++ 2dScroll/src/Window.java
@@ -1,9 +1,11 @@
+import java.awt.Frame;
 
-public class Window {
+public class Window extends Frame{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
 	}
 
 }
