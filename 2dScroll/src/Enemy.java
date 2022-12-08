5d0bc5419e313fb1e909a295898df1b2fcfb1568
Ricard Skrudlik <ricardskrudlik34@gmail.com>
2022-12-08 20:18:21 +0100

    Created enemy class

---
+++ 2dScroll/src/Enemy.java
@@ -0,0 +1,31 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Enemy {
+	
+	int x,y;
+	int v = 8;
+	int width = 30;
+	int height = 30;
+
+	Enemy(int x, int y, int v) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	void move() {
+		x += v;
+	}
+	
+	void attack() {
+		
+	}
+	
+	void paint(Graphics gr) {
+		
+		gr.setColor(Color.RED);
+		gr.drawRect(x, y, width, height);
+		gr.drawLine((int)(x + width*0.75), y,(int)(x + width*0.75), (int)(y+height));
+	}
+
+}
